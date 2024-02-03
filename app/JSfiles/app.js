8c49e74105c5f6e791f5daf4548055c55aa0a504
Dmitry Nihrei <dmitrynihrei@gmail.com>
2024-02-03 16:58:08 +0200

    first commit

---
+++ app/JSfiles/app.js
@@ -0,0 +1,19 @@
+class Task {
+  constructor(title, description) {}
+}
+
+class PopUp {
+  popUpHTNL = {}
+
+  create() {}
+
+  close() {}
+
+  delete() {}
+}
+
+class TaskDescription extends PopUp {
+  constructor(description) {}
+}
+
+class MakeTask extends PopUp {}
